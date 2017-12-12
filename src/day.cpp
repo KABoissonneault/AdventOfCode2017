f9d3f88836ac35ba3469e9bcecca94468337ed7b
KABoissonneault <kaboissonneault@gmail.com>
2017-12-12 11:50:13 -0500

    Solved day12

--- src/day.cpp
+++ src/day.cpp
@@ -13,6 +13,7 @@
 #include "error.h"
 #include "conversion.h"
 #include "parser.h"
+#include "disjoint_set.h"
 #include <fstream>
 
 #if defined(_MSC_VER)
@@ -1698,6 +1699,182 @@ namespace kab_advent {
 				}
 			}
 		}
+
+		namespace day12 {
+			struct program {
+				int id;
+				std::vector<int> links;
+			};
+
+			using input_t = std::vector<program>;
+
+			auto consume_newline( std::string_view & line ) -> bool {
+				if ( !line.empty() && line.front() == '\n' ) {
+					line.remove_prefix( 1 );
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			auto consume_delimiter( std::string_view & line ) -> bool {
+				if ( !line.empty() && line.front() == ',' ) {
+					line.remove_prefix( 1 );
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			auto consume_arrow( std::string_view & line ) -> bool {
+				line = left_trim( line );
+				if ( begins_with( line, "<->" ) ) {
+					line.remove_prefix( 3 );
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			auto parse_program_id_list( std::string_view line ) -> expected<parsed_value<std::vector<int>>> {
+				auto ids = std::vector<int>();
+				do {
+					line = left_trim( line );
+
+					auto const program_id_result = to_int( line );
+					if ( !program_id_result ) {
+						return make_unexpected( program_id_result.error() );
+					}
+
+					ids.push_back( program_id_result.value().data );
+					line = line.substr( std::distance( line.data(), program_id_result.value().conversion_end ) );
+				} while ( consume_delimiter( line ) );
+
+				return parsed_value<std::vector<int>>{ ids, line };
+			}
+
+			auto parse_program( std::string_view line ) -> expected<parsed_value<program>> {
+				auto const program_id_result = to_int( line );
+				if ( !program_id_result ) {
+					return make_unexpected( program_id_result.error() );
+				}
+
+				line = line.substr(std::distance(line.data(), program_id_result.value().conversion_end));
+				if ( !consume_arrow( line ) ) {
+					return make_unexpected( error_info( std::make_error_code( std::errc::invalid_argument ), "Expected \"<->\" after program id" ) );
+				}
+
+				auto const links_result = parse_program_id_list( left_trim( line ) );
+				if ( !links_result ) {
+					return make_unexpected( links_result.error() );
+				}
+				line = links_result.value().rest_instruction;
+
+				return parsed_value<program>{ program { program_id_result.value().data, links_result.value().value }, line };
+			}
+
+			auto parse_programs( std::string_view line ) -> expected<input_t> {
+				auto in = input_t();
+				do {
+					line = left_trim( line );
+					auto const program_result = parse_program( line );
+					if ( !program_result ) {
+						return make_unexpected( program_result.error() );
+					}
+
+					in.push_back( program_result.value().value );
+					line = program_result.value().rest_instruction;
+				} while ( consume_newline( line ) && !line.empty() );
+				return in;
+			}
+
+			auto input( gsl::span<std::string_view const> args ) -> expected<input_t> {
+				if ( args.size() == 0 ) {
+					return make_unexpected( error_info( std::make_error_code( std::errc::invalid_argument ), "Stdin input not supported for this day" ) );
+				} else if ( args[0] == "--input" ) {
+					return make_unexpected( error_info( std::make_error_code( std::errc::invalid_argument ), "--input not supported for this day" ) );
+				} else if ( args[0] == "--file" ) {
+					if ( args.size() < 2 ) {
+						return make_unexpected( error_info( std::make_error_code( std::errc::invalid_argument ), "Missing filename after --input" ) );
+					}
+
+					auto const filepath = args[1];
+					auto file = std::ifstream( std::string( filepath ) );
+					if ( !file ) {
+						return make_unexpected( error_info( std::make_error_code( std::errc::invalid_argument ), "File \""s.append( filepath ).append( "\" could not be opened" ) ) );
+					}
+
+					auto input = std::string();
+					auto line = std::string();
+					while ( std::getline( file, line ) ) {
+						if ( !line.empty() ) {
+							input.append( line ).append("\n");
+						}
+					}
+					
+					return parse_programs(input);
+				} else {
+					return make_unexpected(
+						error_info( std::make_error_code( std::errc::invalid_argument ), "Invalid parameter \""s.append( args[0] ).append( "\"" ) )
+					);
+				}
+			}
+
+			auto make_set( input_t const& in ) -> disjoint_set<int> {
+				auto s = disjoint_set<int>();
+				for ( auto const& e : in ) {
+					s.add_element( e.id );
+				}
+
+				for ( auto const& e : in ) {
+					for ( auto const& link : e.links ) {
+						s.unite( e.id, link );
+					}
+				}
+				return s;
+			}
+
+			auto part1( input_t in ) -> int {
+				auto s = make_set( in );
+
+				return std::count_if( in.begin(), in.end(), [&s, zero_root = s.find_root( in[0].id )]( program const& p ) -> bool { return s.find_root( p.id ) == zero_root; } );
+			}
+
+			auto part2( input_t in ) -> int {
+				auto s = make_set( in );
+
+				auto group_ids = std::set<int>();
+				for ( auto const& e : in ) {
+					group_ids.emplace( s.find_root( e.id ) );
+				}
+
+				return group_ids.size();
+			}
+
+			auto solve( gsl::span<std::string_view const> args ) -> int {
+				if ( args.size() < 1 ) {
+					throw std::runtime_error( "Missing part parameter" );
+				}
+				auto const part = args[0];
+				args = args.subspan( 1 );
+
+				auto const in = input( args );
+				if ( !in ) {
+					std::cerr << in.error() << "\n";
+					return EXIT_FAILURE;
+				}
+
+				if ( part == "1" ) {
+					std::cout << part1( std::move( in ).value() ) << "\n";
+					return EXIT_SUCCESS;
+				} else if ( part == "2" ) {
+					std::cout << part2( std::move( in ).value() ) << "\n";
+					return EXIT_SUCCESS;
+				} else {
+					throw std::runtime_error { "Parameter \""s.append( part ).append( "\" was not a valid part (try 1 or 2)" ) };
+				}
+			}
+		}
     }
 
     auto day(gsl::span<std::string_view const> args) -> int {
@@ -1729,6 +1906,8 @@ namespace kab_advent {
             return day10::solve(args);
         } else if ( day == "11" ) {
 			return day11::solve( args );
+		} else if ( day == "12" ) {
+			return day12::solve( args );
 		} else {
             throw std::runtime_error{"Parameter \""s.append(day).append("\" was not a valid day (try 1-25)")};
         }
