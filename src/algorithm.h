8dc145b49780ea79f47622caafa845dbe8d84447
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2017-12-14 20:11:41 -0500

    Hex char to bit string

--- src/algorithm.h
+++ src/algorithm.h
@@ -267,4 +267,38 @@ namespace kab_advent {
 	inline auto begins_with( std::string_view s, std::string_view v ) -> bool {
 		return s.compare( 0, v.size(), v ) == 0;
 	}
+
+    inline const char* hex_char_to_bin(char c) {
+        switch(toupper(c)) {
+            case '0': return "0000";
+            case '1': return "0001";
+            case '2': return "0010";
+            case '3': return "0011";
+            case '4': return "0100";
+            case '5': return "0101";
+            case '6': return "0110";
+            case '7': return "0111";
+            case '8': return "1000";
+            case '9': return "1001";
+            case 'A': 
+            case 'a':
+                return "1010";
+            case 'B': 
+            case 'b':
+                return "1011";
+            case 'C':
+            case 'c':
+                return "1100";
+            case 'D':
+            case 'd':
+                return "1101";
+            case 'E':
+            case 'e':
+                return "1110";
+            case 'F':
+            case 'f':
+                return "1111";
+        }
+        throw std::runtime_error("Invalid char");
+    }
 }
